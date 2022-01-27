Found bug in baseline_grid_locations. Derivatives now off by a max of 1 due to the use of a fix and/or floor function. visibility_degrid runs and completes but a row is missing, still investigating.

fhd_core/gridding/baseline_grid_locations.py
@@ -66,7 +66,6 @@ def baseline_grid_locations(obs, psf, params, vis_weights, bi_use = None, fi_use
         fi_use = np.nonzero(b_info[0]['freq_use'][0])[0]
     frequency_array = b_info[0]['freq'][0]
     frequency_array = frequency_array[fi_use]
-    n_freq_use = frequency_array.size
 
     # At this point the vis_weight_switch is configured, 
     # but there's no point to us doing that because its never a pointer!
@@ -128,7 +127,7 @@ def baseline_grid_locations(obs, psf, params, vis_weights, bi_use = None, fi_use
     if interp_flag:
         # Derivatives from pixel edge to baseline center for use in interpolation
         dx_arr = (xcen - np.floor(xcen)) * psf_resolution - np.floor((xcen - np.floor(xcen)) * psf_resolution)
-        dy_arr = (xcen - np.floor(ycen)) * psf_resolution - np.floor((ycen - np.floor(ycen)) * psf_resolution)
+        dy_arr = (ycen - np.floor(ycen)) * psf_resolution - np.floor((ycen - np.floor(ycen)) * psf_resolution)
         baselines_dict['dx0dy0_arr'] = (1 - dx_arr) * (1 - dy_arr)
         baselines_dict['dx0dy1_arr'] = (1 - dx_arr) * dy_arr
         baselines_dict['dx1dy0_arr'] = dx_arr * (1 - dy_arr)

fhd_core/gridding/visibility_degrid.py
@@ -41,7 +41,7 @@ def visibility_degrid(image_uv, vis_weights, obs, psf, params, polarization = 0,
         [description], by default False
     """
 
-    complex = psf['complex_flag'][0]
+    complex_flag = psf['complex_flag'][0]
     n_spectral = obs['degrid_spectral_terms'][0]
     interp_flag = psf['interpolate_kernel'][0]
     if conserve_memory:
@@ -119,10 +119,10 @@ def visibility_degrid(image_uv, vis_weights, obs, psf, params, polarization = 0,
     
     ind_ref = np.arange(max(bin_n))
 
-    if complex:
-        arr_type = complex
+    if complex_flag:
+        arr_type = np.cdouble
     else:
-        arr_type = float
+        arr_type = np.double
     
     if n_spectral:
         prefactor = np.zeros(n_spectral)
@@ -140,13 +140,12 @@ def visibility_degrid(image_uv, vis_weights, obs, psf, params, polarization = 0,
             mem_iter = int(np.ceil(required_bytes / memory_threshold))
         else:
             mem_iter = 1
-        # These variables will get used in the case of mem_iter > 1
-        vis_n_full = vis_n
-        inds_full = inds
-        vis_n_per_iter = np.ceil(vis_n_full/mem_iter)
         # loop over chunks of visibilities to grid to conserve memory
         for mem_i in range(mem_iter):
             if mem_iter > 1:
+                vis_n_full = vis_n
+                inds_full = inds
+                vis_n_per_iter = np.ceil(vis_n_full/mem_iter)
                 # calculate the indices of this visibility chunk if split into multiple chunks
                 if vis_n_per_iter * (mem_i + 1) > vis_n_full:
                     max_ind = vis_n_full