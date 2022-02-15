11264827516bae88d3e02e74c5105f1f375c73ed
SkyWa7ch3r <joel.g.dunstan@curtin.edu.au>
2022-02-15 11:27:43 +0800

    ADC-500205 #comment Added python badge, environment.yml. ADC-500242 #in-progress #comment package is now available on the command line, argparse will mostly replace fhd_setup.

---
+++ PyFHD/pyfhd.py
@@ -0,0 +1,6 @@
+import numpy as np
+import astropy 
+import argparse
+
+def main():
+    print("Hi, I'm the main PyFHD function, if you're seeing me, well it's good, just trust me, it's good.")
--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = "PyFHD",
     version = 1.0,
     author = "ADACS - Astronomy Data and Compute Services",
-    description = "Python Fast Holograhic Deconvolution",
-    url = "",
+    url = "https://github.com/ADACS-Australia/PyFHD",
     python_requires=">=3.7",
     packages = ['PyFHD'],
+    description = 'Python Fast Holograhic Deconvolution: A Python package that does efficient image deconvolution for general radio astronomy, fast-mode Epoch of Reionization analysis, and simulation.',
+    long_description = open("README.md").read(),
+    long_description_content_type = 'text/markdown',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts' : ['pyfhd = PyFHD.pyfhd:main'],
+    }
 )
 
 #TODO: Add More Fields and complete Setup.py
