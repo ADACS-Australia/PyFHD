5f5885ec4a1b6dba3cf9032e23b4f73a24cfb34c
SkyWa7ch3r <joel.g.dunstan@curtin.edu.au>
2022-04-14 15:57:34 +0800

    Finding arguments for argparse...standby

--- PyFHD/pyfhd.py
+++ PyFHD/pyfhd.py
@@ -13,21 +13,17 @@ def pyfhd_parser():
         args_for_setting_config_path = ['-c', '--config'],
 
     )
-    parser.add_argument('--test', help = "Test argument", action="store_true")
+    parser.add_argument('obs_id', help="The Observation ID as per the MWA file naming standards")
+    parser.add_argument( '-u', '--uvfits-path', help = "Directory for the uvfits files, by default it looks for a directory called uvfits in the working directory", default = "./uvfits/")
+    parser.add_argument('-o','--output-path', help = "Set the output path for the current run, note a directory will still be created inside the given path")
+    parser.add_argument('-d', '--description', help = "A more detailed description of the current task, will get applied to the output directory and logging where all output will be stored.")
     parser.add_argument('--stringy', type = str)
-    parser.add_argument('--test2', help = "Test argument 2", action="store_true")
-    parser.add_argument('-p', '--path', type = Path)
+    parser.add_argument('--test-arg', help = "Test argument 2")
+    #parser.add_argument('-p', '--path', type = Path)
 
     return parser
 
 def main():
     options = pyfhd_parser().parse_args()
-    print(options)
-    print(Path.cwd())
-    if options.test:
-        print("Hello world!")
-        print(options.stringy)
-        print(options.path)
-    else:
-        print("Fail On Purpose World!")
     print("Hi, I'm the main PyFHD function, if you're seeing me, well it's good, just trust me, it's good.")
+    print(options)
