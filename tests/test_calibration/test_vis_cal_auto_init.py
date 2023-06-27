82ee4dc69d66c15e8160406d79c27027b7b26739
JLBLine <jack.l.b.line@gmail.com>
2023-06-27 14:23:22 +0800

    `vis_cal_auto_init` now passes a test

--- tests/test_calibration/test_vis_cal_auto_init.py
+++ tests/test_calibration/test_vis_cal_auto_init.py
@@ -8,72 +8,18 @@ from PyFHD.pyfhd_tools.test_utils import recarray_to_dict, sav_file_vis_arr_swap
 
 import numpy as np
 import deepdish as dd
+import numpy.testing as npt
 
 @pytest.fixture
 def data_dir():
     return Path(env.get('PYFHD_TEST_PATH'), "vis_cal_auto_init")
 
-@pytest.fixture
-def base_dir():
-    return Path(env.get('PYFHD_TEST_PATH'))
-
-# def test_calc_adapt_gain_one(data_dir):
-#     gain_list, convergence_list, iter, base_gain, final_con_est, expected_gain = get_data_items(
-#         data_dir,
-#         'input_gain_list_1.npy',
-#         'input_convergence_list_1.npy',
-#         'input_iter_1.npy',
-#         'input_base_gain_1.npy',
-#         'input_final_convergence_estimate_1.npy',
-#         'output_gain_1.npy'
-#     )
-#     result_gain = vis_cal_auto_init(gain_list, convergence_list, iter, base_gain, final_convergence_estimate = final_con_est)
-#     assert expected_gain == result_gain
-
-# def test_calc_adapt_gain_two(data_dir):
-#     gain_list, convergence_list, iter, base_gain, final_con_est, expected_gain = get_data_items(
-#         data_dir,
-#         'input_gain_list_2.npy',
-#         'input_convergence_list_2.npy',
-#         'input_iter_2.npy',
-#         'input_base_gain_2.npy',
-#         'input_final_convergence_estimate_2.npy',
-#         'output_gain_2.npy'
-#     )
-#     result_gain = vis_cal_auto_init(gain_list, convergence_list, iter, base_gain, final_convergence_estimate = final_con_est)
-#     assert expected_gain == result_gain
-
-# '''
-# The below test will never pass due to IDL's default median behaviour, for example
-# PRINT, MEDIAN([1, 2, 3, 4], /EVEN) 
-# 2.50000
-# PRINT, MEDIAN([1, 2, 3, 4])
-# 3.00000
-
-# One has to use the EVEN keyword to get the *proper* median behaviour that most mathematicians and scientists
-# expect. This was the only thing that produced the wrong results was the use of numpy median instead of IDL MEDIAN.
-# The array that went into it est_final_conv produced the exact same results in Python and IDL (with the exception of
-# Python producing a more accurate result due to double precision) 
-
-# def test_calc_adapt_gain_three(data_dir):
-#     gain_list, convergence_list, iter, base_gain, expected_gain = get_data_items(
-#         data_dir,
-#         'input_gain_list_3.npy',
-#         'input_convergence_list_3.npy',
-#         'input_iter_3.npy',
-#         'input_base_gain_3.npy',
-#         'output_gain_3.npy'
-#     )
-#     result_gain = vis_cal_auto_init(gain_list, convergence_list, iter, base_gain)
-#     assert expected_gain == result_gain
-# '''
-
-def test_pointsource1_vary(data_dir):
+def run_test(data_dir, tag_name):
     """Runs the test on `vis_cal_auto_init` - reads in the data in `data_loc`,
     and then calls `vis_cal_auto_init`, checking the outputs match expectations"""
 
-    h5_before = dd.io.load(Path(data_dir, "before_vis_cal_auto_init.h5"))
-    h5_after = dd.io.load(Path(data_dir, "after_vis_cal_auto_init.h5"))
+    h5_before = dd.io.load(Path(data_dir, f"{tag_name}_before_vis_cal_auto_init.h5"))
+    h5_after = dd.io.load(Path(data_dir, f"{tag_name}_after_vis_cal_auto_init.h5"))
 
     obs = h5_before['obs']
     cal = h5_before['cal']
@@ -83,23 +29,32 @@ def test_pointsource1_vary(data_dir):
     vis_model_auto = h5_before['vis_model_auto']
     auto_tile_i = h5_before['auto_tile_i']
 
-    expected_cal = h5_after['cal_init']
-    expected_auto_gain = sav_file_vis_arr_swap_axes(expected_cal['gain'])
-
-    ##TODO this is failing, there are a couple of translation boo-boos
+    expected_auto_gain = h5_after['auto_gain']
 
     result_auto_gain = vis_cal_auto_init(obs, cal, vis_arr, vis_model_arr, vis_auto, vis_model_auto, auto_tile_i)
 
     ##Check returned gain is as expected
-    assert np.allclose(result_auto_gain, expected_auto_gain, atol=1e-4)
+    npt.assert_allclose(result_auto_gain, expected_auto_gain, atol=1e-4)
+
+# def test_pointsource1_standard(data_dir):
+#     """Test using the `pointsource1_standard` set of inputs"""
+
+#     run_test(data_dir, "pointsource1_standard")
+    
+def test_pointsource2_vary1(data_dir):
+    """Test using the `pointsource1_standard` set of inputs"""
+
+    run_test(data_dir, "pointsource2_vary1")
 
 if __name__ == "__main__":
 
-    def convert_before_sav(test_dir):
+    def convert_before_sav(data_dir, tag_name):
         """Takes the before .sav file out of FHD function `vis_cal_auto_init`
         and converts into an hdf5 format"""
 
-        sav_dict = convert_sav_to_dict(f"{test_dir}/before_vis_cal_auto_init.sav", "meh")
+        func_name = 'vis_cal_auto_init'
+
+        sav_dict = convert_sav_to_dict(f"{data_dir}/{tag_name}_before_{func_name}.sav", "meh")
 
         obs = recarray_to_dict(sav_dict['obs'])
         cal = recarray_to_dict(sav_dict['cal'])
@@ -116,31 +71,33 @@ if __name__ == "__main__":
 
         h5_save_dict['auto_tile_i'] = sav_dict['auto_tile_i']
         
-        dd.io.save(Path(test_dir, "before_vis_cal_auto_init.h5"), h5_save_dict)
+        dd.io.save(Path(data_dir, f"{tag_name}_before_{func_name}.h5"), h5_save_dict)
         
-    def convert_after_sav(test_dir):
+    def convert_after_sav(data_dir, tag_name):
         """Takes the after .sav file out of FHD function `vis_cal_auto_init`
         and converts into an hdf5 format"""
 
-        sav_dict = convert_sav_to_dict(f"{test_dir}/after_vis_cal_auto_init.sav", "meh")
-        
-        cal_init = recarray_to_dict(sav_dict["cal_init"])
+        func_name = 'vis_cal_auto_init'
 
+        sav_dict = convert_sav_to_dict(f"{data_dir}/{tag_name}_after_{func_name}.sav", "meh")
+        
         ##super dictionary to save everything in
         h5_save_dict = {}
-        h5_save_dict['cal_init'] = cal_init
+        h5_save_dict['auto_gain'] = sav_file_vis_arr_swap_axes(sav_dict["auto_gain"])
         
-        dd.io.save(Path(test_dir, "after_vis_cal_auto_init.h5"), h5_save_dict)
+        dd.io.save(Path(data_dir, f"{tag_name}_after_{func_name}.h5"), h5_save_dict)
         
-    def convert_sav(test_dir):
+    def convert_sav(base_dir, tag_name):
         """Load the inputs and outputs needed for testing `vis_cal_auto_init`"""
-        convert_before_sav(test_dir)
-        convert_after_sav(test_dir)
+        convert_before_sav(base_dir, tag_name)
+        convert_after_sav(base_dir, tag_name)
 
     ##Where be all of our data
-    base_dir = Path(env.get('PYFHD_TEST_PATH'))
+    base_dir = Path(env.get('PYFHD_TEST_PATH'), 'vis_cal_auto_init')
 
     ##Each test_set contains a run with a different set of inputs/options
-    for test_set in ['pointsource1_vary1']:
-        convert_sav(Path(base_dir, test_set))
-        # run_test(Path(base_dir, test_set))
+    tag_names = ['pointsource2_vary1']
+
+    for tag_name in tag_names:
+        convert_sav(base_dir, tag_name)
+        # run_test(base_dir, tag_name)
