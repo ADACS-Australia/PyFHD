fb546302384b749c87944b250f9f2c911756834f
thea <thiasnyder@gmail.com>
2026-07-30 11:07:15 -0700

    got rid of breakpoint

--- src/pyfhd/beam_setup/beam_utils.py
+++ src/pyfhd/beam_setup/beam_utils.py
@@ -166,7 +166,6 @@ def beam_image(
     group_id = (psf["id"][pol_i, 0, :]).astype(int)
     # IDL psf structures often have "beam_gaussian_params" defined but set to zero
     # have to test that it's present *and* nonzero
-    # breakpoint()
     if "beam_gaussian_params" in psf and (psf["beam_gaussian_params"] != 0).all():
         beam_gaussian_params = psf["beam_gaussian_params"][:]
     else:
